49317ad30987c996b4e70f15e5908f199c3dd0f7
Michael Tona <149912895+bytemyke@users.noreply.github.com>
2024-05-19 12:47:57 -0400

    Initial creation from template

---
+++ src/sprites/GoodTarget.tsx
@@ -0,0 +1,13 @@
+import { Sprite } from "@pixi/react";
+import img from "../assets/goodTarget.png"
+export function GettingReadyScreen() {
+    return (
+      <>
+        <Sprite
+        
+        >
+
+        </Sprite>
+      </>
+    )
+  }
